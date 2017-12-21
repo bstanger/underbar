de4fe36e261d0e522c860de53554bbc5269e8607
Becca Stanger <beccastanger@gmail.com>
2017-12-21 15:07:09 -0800

    Add test data for uniq
    This work adds the missing FILL_ME_IN value for a uniq
    test.  It also adds two more minor tests to the uniq
    tests to ensure that sorted collections without an iterator
    work and that isSorted parameter is verified.

--- spec/part1.js
+++ spec/part1.js
@@ -346,7 +346,20 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
+      });
+
+
+      it('should handle sorted arrays without iterators', function() {
+        var numbers = [40, 42, 42, 53, 55, 55];
+
+        expect(_.uniq(numbers, true)).to.eql([40, 42, 53, 55]);
+      });
+
+      it('should verify isSorted parameter is correct', function() {
+        var numbers = [46, 42, 42, 53, 55, 55];
+
+        expect(_.uniq(numbers, true)).to.eql("Error - isSorted parameter is incorrect");
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
